1383f02d911326a8bdb567deb132bf5e9214f538
robertof1lho <159061352+robertof1lho@users.noreply.github.com>
2024-02-08 13:50:14 -0300

    Add files via upload

---
+++ novoArquivo.js
@@ -0,0 +1,23 @@
+var nota1 = 3;
+var nota2 = 9;
+var nota3 = 10;
+var soma = (nota1+nota2+nota3);
+var media = (soma/3);
+
+console.log("Nota 1 ->" , nota1);
+console.log("Nota 2 ->" , nota2);
+console.log("Nota 3 ->" , nota3);
+
+if (media >= 7){
+    console.log("Aluno aprovado!");
+ } else if (media <= 4) {
+    console.log("Aluno reprovado.");
+ } else {
+    console.log("Aluno em recuperação.");
+ }
+
+
+
+
+
+
